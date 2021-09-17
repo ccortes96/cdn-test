b0f4cefb14f2921528e771ed4cbdb14b2d8be446
Cristian Cortés <cristiancortes9645@gmail.com>
2021-09-17 15:45:21 -0600

    :rocket: V0.0.3

---
+++ assets/index.5376d4ca.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.c803325f.js";var l=()=>e.createElement("div",null,e.createElement("h1",null,"Hello Fun"));export{l as default};
--- assets/index.58e222f8.js
+++
@@ -1,4 +0,0 @@
-import { R as e } from 'https://cdn.jsdelivr.net/gh/ccortes96/cdn-test@v0.0.2/assets/vendor.0046c8f2.js'
-var l = () =>
-  e.createElement('div', null, e.createElement('h1', null, 'Hello Fun'))
-export { l as default }
