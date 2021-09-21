4d71eb1f4399cb41d0665c3aa1a62e429754a53e
Cristian Cortés <cristiancortes9645@gmail.com>
2021-09-21 09:52:49 -0600

    :rocket: v0.0.6

--- assets/index.5376d4ca.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./vendor.c803325f.js";var l=()=>e.createElement("div",null,e.createElement("h1",null,"Hello Fun"));export{l as default};
---
+++ assets/index.58e222f8.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.0046c8f2.js";var l=()=>e.createElement("div",null,e.createElement("h1",null,"Hello Fun"));export{l as default};
