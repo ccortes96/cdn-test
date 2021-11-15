1fe3d8c5654ec0c0ba2ff4105bac8c9b314b2862
Cristian Cortés <cristiancortes9645@gmail.com>
2021-11-15 15:10:53 -0600

    v0.0.9

--- assets/index.58e222f8.js
+++
@@ -1,1 +0,0 @@
-import{R as e}from"./vendor.0046c8f2.js";var l=()=>e.createElement("div",null,e.createElement("h1",null,"Hello Fun"));export{l as default};
---
+++ index.a4510e55.js
@@ -0,0 +1,1 @@
+import{R as e}from"./vendor.d30c2f04.js";var l=()=>e.createElement("div",null,e.createElement("h1",null,"Hello Fun"));export{l as default};
